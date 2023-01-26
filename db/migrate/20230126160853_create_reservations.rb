6aa948a3d0fe2a046fb0a574ad35752db16d033a
Vinod Lala <vinodlala@users.noreply.github.com>
2023-01-26 11:09:29 -0500

    Generate Reservation model

---
+++ db/migrate/20230126160853_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.string :contact_email
+      t.string :contact_phone
+      t.string :party_name
+      t.integer :party_size
+      t.datetime :reservation_starts_at
+
+      t.timestamps
+    end
+  end
+end
